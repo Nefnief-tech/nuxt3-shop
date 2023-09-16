3b8953f7cf5043a46959ac9ffd6ce5fa92c7343b
Nefnief-tech <104936980+Nefnief-tech@users.noreply.github.com>
2023-09-17 00:23:42 +0200

    cards-added with grid and some style

--- components/cards.vue
+++ components/cards.vue
@@ -1,7 +1,74 @@
 <template>
-  <!--card-1-->
-  <div class="card bg-neutral-800 w-1/3 rounded-3xl">
-    <!--card headdar-->
-    <h1 class="text-center text-white">Minecraft</h1>
+  <!--grid-->
+  <div class="grid grid-cols-3 center gap-10 ml-10 mr-10">
+    <!--card-1-->
+    <div class="card bg-neutral-800 w-1/2 rounded-3xl mt-10">
+      <!--card headdar-->
+      <div class="center">
+        <Icon
+          name="file-icons:minecraft"
+          color="white"
+          size="5rem"
+          class="pt-5"
+        />
+      </div>
+      <p class="text-white text-center p-10">
+        with our cheap and performant server we are the best partner you coul
+        imagine with a starting price of only 1.50€ per month you can get your
+        own server with 1GB of ram and 1 vCore. If you want to get more power
+        you can upgrade your server at any time. We also offer a free 7 day
+        trial for our servers so you can test them out before you buy them. If
+        you have any questions you can contact us at any time via our discord
+        server or via email. We are looking forward to your visit.
+      </p>
+    </div>
+
+    <!--card-2-->
+
+    <div class="card bg-neutral-800 w-1/2 rounded-3xl mt-10">
+      <!--card headdar-->
+      <div class="center">
+        <Icon
+          name="solar:server-minimalistic-broken"
+          color="white"
+          size="5rem"
+          class="pt-5"
+        />
+      </div>
+      <p class="text-white text-center p-10">
+        with the power of enterprise hardware and the latest technology we are
+        able to offer you the best performance for your server. We also offer a
+        free 7 day trial for our servers so you can test them out before you buy
+        them. If you have any questions you can contact us at any time via our
+        discord server or via email. We are looking forward to your visit.
+      </p>
+    </div>
+
+    <!--card-3-->
+
+    <div class="card bg-neutral-800 w-1/2 rounded-3xl mt-10">
+      <!--card headdar-->
+      <div class="center">
+        <Icon
+          name="material-symbols:shield-locked-rounded"
+          color="white"
+          size="5rem"
+          class="pt-5"
+        />
+      </div>
+      <p class="text-white text-center p-10">
+        with our secure and encrypted network we can guarantee you the best
+        protection for your server. We also offer a free 7 day trial for our
+        servers so you can test them out before you buy them. If you have any
+        questions you can contact us at any time via our discord server or via
+        email. We are looking forward to your visit.
+      </p>
+    </div>
   </div>
 </template>
+<style>
+.center {
+  justify-content: center;
+  display: flex;
+}
+</style>
